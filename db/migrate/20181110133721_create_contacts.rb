97a6e9f137b86f8121e694160d7870a176101c13
Maria Castro <mariacastro@aquaporservicos.pt>
2018-11-10 13:40:40 +0000

    12 contact model created with name/address/mail/phone/schedule

---
+++ db/migrate/20181110133721_create_contacts.rb
@@ -0,0 +1,13 @@
+class CreateContacts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.string :address
+      t.string :email
+      t.string :phone_number
+      t.string :schedule
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_10_124444) do
+ActiveRecord::Schema.define(version: 2018_11_10_133721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "email"
+    t.string "phone_number"
+    t.string "schedule"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
