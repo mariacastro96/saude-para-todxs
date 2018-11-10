18f50d386e7fc323ede9d4b1357fa7a1a841c306
Maria Castro <mariacastro@aquaporservicos.pt>
2018-11-10 23:47:50 +0000

    #28 CRUD statements

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -1,3 +1,25 @@
 class Statement < ApplicationRecord
+  # Includes
+
+  # Attributes
+
+  # Associations
   belongs_to :language
+
+  # Delegates
+
+  # Constants
+
+  # Validations
+  validates :code, :title, :text, :language_id, presence: true
+  
+  # Scopes
+
+  # Callbacks
+
+  # Constants Methods
+
+  # Default
+
+  private
 end
