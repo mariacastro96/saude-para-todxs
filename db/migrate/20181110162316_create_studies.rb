ff0359326ccd5409ae0f87e47f14963fb86c3b64
Maria Castro <mariacastro@aquaporservicos.pt>
2018-11-11 23:48:14 +0000

    minor fix

--- db/migrate/20181110162316_create_studies.rb
+++ db/migrate/20181110162316_create_studies.rb
@@ -1,20 +1,12 @@
 class CreateStudies < ActiveRecord::Migration[5.2]
   def change
     create_table :studies do |t|
-<<<<<<< HEAD:db/migrate/20181110162316_create_studies.rb
-      t.string :code
-      t.string :title
-      t.text :description
-=======
       t.string :code, null: false
       t.string :title, null: false
-      t.string :subtitle
-      t.integer :total_value, null: false
->>>>>>> origin/master:db/migrate/20181110162031_create_studies.rb
+      t.text :description
       t.references :language, foreign_key: true
 
       t.timestamps
     end
   end
 end
-  
